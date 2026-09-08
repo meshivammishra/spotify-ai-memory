0ca52992f49a2a9b73faea548e2bc82417cb9d88
Shivam Mishra <shivammishra2872@gmail.com>
2026-09-08 15:18:45 +0530

    Update embedder.py

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -1,8 +1,16 @@
-from sentence_transformers import SentenceTransformer
+import os
+from google import genai
+from google.genai import types
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def create_embedding(text: str):
-    embedding = model.encode(text)
-    return embedding.tolist()
+    result = client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=text,
+        config=types.EmbedContentConfig(
+            output_dimensionality=384
+        ),
+    )
+
+    return result.embeddings[0].values
